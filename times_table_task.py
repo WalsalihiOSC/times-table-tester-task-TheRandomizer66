e508999dfb448f872aeec54d3eed73220368d336
Clyde Ivan Fronda <78827014+TheRandomizer66@users.noreply.github.com>
2022-04-05 11:28:02 +1200

    Function + Generating numbers
    I added my function which checks whether the input of the user is the right answer and in addition to that, I have generated the numbers that will be displayed for the user to calculate.

--- times_table_task.py
+++ times_table_task.py
@@ -6,17 +6,27 @@ class MultiplicationCalculator:
     def __init__(self, window):
         f1 = Frame(window)
         f2 = Frame(window)
-        label1 = Label(f1, text="Number x Number =")
-        entry1 = Entry(f1)
+        number1 = rand.randint(0, 15)
+        number2 = rand.randint(0, 15)
+        self.answer = number1*number2
+        label1 = Label(f1, text=f"{number1} x {number2} = ")
+        self.entry1 = Entry(f1)
         button1 = Button(f2, text="Check Answer")
         button2 = Button(f2, text="Next")
+        self.response = Label(f1, text="Please type in your answer")
 
         label1.grid(row=0, column=0)
-        entry1.grid(row=0, column=1)
-        button1.grid(row=0, column=0, sticky=W)
-        button2.grid(row=0, column=1, sticky=E)
+        self.entry1.grid(row=0, column=1)
+        button1.grid(row=0, column=0)
+        button2.grid(row=0, column=1)
         f1.grid(row=0, column=0)
         f2.grid(row=1, column=0)
+
+    def checker(self):
+        if self.entry1.get() == self.answer:
+            self.response.configure(text="Correct!")
+        else:
+            self.response.configure(text="Incorrect!")
 
 
 root = Tk()
